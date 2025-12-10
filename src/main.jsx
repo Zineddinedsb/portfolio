780dcd5ccc2b43020161209aefbb42d61f435029
zineddien <zineddine@gmail.com>
2025-12-10 22:05:37 +0100

    update portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react'; // Importe le mode strict de React
+import { createRoot } from 'react-dom/client'; // Importe createRoot pour le rendu
+import './index.css'; // Importe le fichier CSS pour les styles globaux
+import App from './App.jsx'; // Importe le composant principal App
 
+// Crée une racine pour le rendu
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
